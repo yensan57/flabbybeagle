c0cfe2c2b29bbc253d2f68af227273ab630c7e0d
yensan57 <70277606+yensan57@users.noreply.github.com>
2025-09-28 16:55:54 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// ===== script.js =====
-// You can add JavaScript here later, for example:
-// - Mobile menu toggle
-// - Image sliders
-// - Interactive shop cart
-
-console.log("FlabbyBeagle site loaded!");
