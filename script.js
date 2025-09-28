2b414c29a51d5892bc3dcea272eb95c122e3144b
yensan57 <70277606+yensan57@users.noreply.github.com>
2025-09-28 16:28:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// ===== script.js =====
+// You can add JavaScript here later, for example:
+// - Mobile menu toggle
+// - Image sliders
+// - Interactive shop cart
+
+console.log("FlabbyBeagle site loaded!");
